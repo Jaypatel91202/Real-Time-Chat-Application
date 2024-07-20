020fd1ce0323d300e89f8faab0df6c83f6aaacb6
Jay Patel <94241008+Jaypatel91202@users.noreply.github.com>
2024-07-21 00:48:49 +0530

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,7 +12,14 @@ const router = express.Router()
 //create user api
 router.post('/register',registerUser)
 //check user email
-router.post('/email',checkEmail)
+router.post('/email', (req, res) => {
+    // Handle the request and send a response
+    res.setHeader('Access-Control-Allow-Origin', 'https://real-time-chat-application-client.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.json({ message: 'Email endpoint hit!' });
+});
+
 //check user password
 router.post('/password',checkPassword)
 //login user details
