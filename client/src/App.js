ca7862c08ad412a8b73df7b50adb85555ea0feaf
Jay91202 <jaypatel91202@gmail.com>
2024-07-21 00:21:10 +0530

    upload project

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
+import axios from 'axios'
 
 function App() {
+
+
+  
   return (
    <> 
       <Toaster/>
